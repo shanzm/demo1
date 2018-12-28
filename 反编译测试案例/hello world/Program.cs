fa774c5321d5de9f992aa7cb27529e21baba6b91
shanzm <2351144690@qq.com>
2018-12-28 11:35:51 +0800

    反编译测试

---
+++ 反编译测试案例/hello world/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hello_world
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "i am a student";
+            string[] sArray = str.Split(new char[1] { ' ' });
+            Array.Reverse(sArray);
+            foreach (string item in sArray)
+            {
+                Console.WriteLine(item);
+               
+            }
+            Print p = new hello_world.Print();
+            p.DOPrint();
+
+            int i = 12;
+            Console.WriteLine("i=" + i);
+          
+            Console.ReadKey();
+           
+
+        }
+    }
+}
